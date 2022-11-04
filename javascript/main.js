e04f92f16e2dbd9c5ba98e1f732fe6787c3749b8
JerryWski <jarek_wasilewski@yahoo.com>
2022-11-04 19:21:03 +0100

    working js

--- javascript/main.js
+++ javascript/main.js
@@ -1,11 +1,45 @@
-const lightBtn = document.querySelector('#light');
-const darkBtn = document.querySelector('#dark');
+const lightBtn = document.querySelector("#light");
+const darkBtn = document.querySelector("#dark");
 const radioBtns = document.querySelectorAll(".toggle__wrapper input");
+const bodyPart = document.querySelector("body");
 
+const setDarkMode = () => {
+  bodyPart.classList = "dark";
+  localStorage.setItem("colorMode", "dark");//saving info in localstorage after refresh site
+};
 
-radioBtns.forEach(button => {
-    button.addEventListener('click', event => {
-        darkBtn.checked ? setDarkMode() : setLightMode();
-    })
-    
+const setLightMode = () => {
+  bodyPart.classList = "light";
+  localStorage.setItem("colorMode", "light");//saving info in localstorage after refresh site
+};
+
+const colorModeFromLocalStorage = () => {
+  return localStorage.getItem("colorMode"); //saving info in localstorage after refresh site
+};
+
+const colorModeFromPreferences = () => {
+  return window.matchMedia("(prefers-color-scheme: dark)").matches
+    ? "dark"
+    : "light"; // If preference is set or does not match anything (light is default)
+};
+
+const loadAndUpdateColor = () => {
+  // local storage has precendence over the prefers-color-scheme
+  const color = colorModeFromLocalStorage() || colorModeFromPreferences();
+  color == "dark" ? darkBtn.click() : lightBtn.click();
+};
+
+radioBtns.forEach((button) => {
+  button.addEventListener("click", () => {
+    darkBtn.checked ? setDarkMode() : setLightMode();
+  });
 });
+
+window
+  .matchMedia("(prefers-color-scheme: dark)")
+  .addEventListener("change", (event) => {
+    event.matches ? darkBtn.click() : lightBtn.click();
+  });
+
+// Load the right color on startup - localStorage has precedence
+loadAndUpdateColor();
